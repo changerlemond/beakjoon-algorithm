9245a29c5c1313525e96e922363f5f8f1646c59d
changerlemond <dev.jungclaire@gmail.com>
2023-01-28 00:04:27 +0900

    baekjoon 2741 kotlin

---
+++ src/main/kotlin/conditionalStatements/printN.kt
@@ -0,0 +1,12 @@
+package conditionalStatements
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val number: Int = scanner.nextInt()
+
+    for (number in 1..number) {
+        println(number)
+    }
+}
